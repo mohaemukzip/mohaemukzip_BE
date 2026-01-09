4c38021f854e6f936ee721ab1940fe13d75324f8
조은정/이현 <165488194+E-hyeon@users.noreply.github.com>
2026-01-09 21:30:06 +0900

    chore: swagger 설정

--- src/main/java/com/mohaemukzip/mohaemukzip_be/global/config/SwaggerConfig.java
+++ src/main/java/com/mohaemukzip/mohaemukzip_be/global/config/SwaggerConfig.java
@@ -1,4 +1,39 @@
 package com.mohaemukzip.mohaemukzip_be.global.config;
 
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+import io.swagger.v3.oas.models.servers.Server;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
 public class SwaggerConfig {
+    @Bean
+    public OpenAPI MohaemukzipOpenAPI() {
+        String jwtSchemeName = "JWT TOKEN";
+
+        Info info = new Info()
+                .title("뭐해먹집 API")
+                .description("뭐해먹집 API 명세서")
+                .version("1.0.0");
+
+        // API 요청헤더에 인증정보 포함
+        SecurityRequirement securityRequirement = new SecurityRequirement().addList(jwtSchemeName);
+
+        Components components = new Components()
+                .addSecuritySchemes(jwtSchemeName, new SecurityScheme()
+                        .name(jwtSchemeName)
+                        .type(SecurityScheme.Type.HTTP)
+                        .scheme("bearer")
+                        .bearerFormat("JWT"));
+
+        return new OpenAPI()
+                .addServersItem(new Server().url("/"))
+                .info(info)
+                .addSecurityItem(securityRequirement)
+                .components(components);
+    }
 }
