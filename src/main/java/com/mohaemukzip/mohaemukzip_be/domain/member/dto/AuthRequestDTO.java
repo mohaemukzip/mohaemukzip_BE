103b4cc26ddc54203bfe6a2ff5def49e47799da2
조은정/이현 <165488194+E-hyeon@users.noreply.github.com>
2026-01-14 17:11:35 +0900

    refactor: request body변환 수정

--- src/main/java/com/mohaemukzip/mohaemukzip_be/domain/member/dto/AuthRequestDTO.java
+++ src/main/java/com/mohaemukzip/mohaemukzip_be/domain/member/dto/AuthRequestDTO.java
@@ -1,25 +1,32 @@
 package com.mohaemukzip.mohaemukzip_be.domain.member.dto;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Pattern;
 
 public class AuthRequestDTO {
+    @Schema(description = "로그인 요청")
     public record LoginRequest(
+            @Schema(description = "로그인 ID", example = "test01")
             @NotBlank
             String loginId,
 
+            @Schema(description = "비밀번호", example = "Test1234!")
             @NotBlank
             String password
     ) { }
 
     public record SignUpRequest(
+            @Schema(description = "닉네임", example = "뭐해먹집")
             @NotBlank(message = "닉네임은 필수입니다")
             String nickname,
 
+            @Schema(description = "로그인 ID", example = "test01")
             @NotBlank(message = "아이디는 필수입니다")
             @Pattern(regexp = "^(?=.*[A-Za-z])[A-Za-z\\d]{4,20}$", message = "아이디는 영문과 숫자를 사용해 4자 이상 입력해주세요.")
             String loginId,
 
+            @Schema(description = "비밀번호", example = "Test1234!")
             @NotBlank(message = "비밀번호는 필수입니다")
             @Pattern(
                     regexp = "^(?=.*[A-Za-z])(?=.*\\d)[A-Za-z\\d@$!%*#?&]{10,}$",
