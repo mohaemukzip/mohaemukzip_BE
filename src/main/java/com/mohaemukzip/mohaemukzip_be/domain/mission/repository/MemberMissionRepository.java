ef4418a4c97c2ce9546cd0210010f726b30a8dc3
binsu <tnqkrdl7@naver.com>
2026-01-17 23:42:11 +0900

    Revert "Merge pull request #41 from mohaemukzip/revert-40-feat/39-home"
    This reverts commit 7068390fc4c5b5934b99912da8ca5bae8ff96834, reversing
    changes made to 938967cb883e3d4c5e6d5e1f529e87755551436f.

--- src/main/java/com/mohaemukzip/mohaemukzip_be/domain/mission/repository/MemberMissionRepository.java
+++ src/main/java/com/mohaemukzip/mohaemukzip_be/domain/mission/repository/MemberMissionRepository.java
@@ -3,5 +3,10 @@ package com.mohaemukzip.mohaemukzip_be.domain.mission.repository;
 import com.mohaemukzip.mohaemukzip_be.domain.mission.entity.MemberMission;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
+
 public interface MemberMissionRepository extends JpaRepository<MemberMission,Long> {
+
+    boolean existsByMemberIdAndMissionIdAndIsCompletedTrue(Long memberId, Long id);
 }
