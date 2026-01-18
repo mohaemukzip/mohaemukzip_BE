04222ade84f1a70bb05081d7dd09b9dcf33b62e0
minjeong <litt22@g.hongik.ac.kr>
2026-01-18 23:01:16 +0900

    fix: 사용자 검증 방식 추가 + 코드리뷰 반영

--- src/main/java/com/mohaemukzip/mohaemukzip_be/domain/ingredient/controller/FridgeController.java
+++ src/main/java/com/mohaemukzip/mohaemukzip_be/domain/ingredient/controller/FridgeController.java
@@ -5,7 +5,6 @@ import com.mohaemukzip.mohaemukzip_be.domain.ingredient.dto.IngredientRequestDTO
 import com.mohaemukzip.mohaemukzip_be.domain.ingredient.dto.IngredientResponseDTO;
 import com.mohaemukzip.mohaemukzip_be.domain.ingredient.service.IngredientCommandService;
 import com.mohaemukzip.mohaemukzip_be.domain.ingredient.service.IngredientQueryService;
-import com.mohaemukzip.mohaemukzip_be.domain.member.entity.Member;
 import com.mohaemukzip.mohaemukzip_be.global.response.ApiResponse;
 import com.mohaemukzip.mohaemukzip_be.global.security.CustomUserDetails;
 import io.swagger.v3.oas.annotations.Operation;
