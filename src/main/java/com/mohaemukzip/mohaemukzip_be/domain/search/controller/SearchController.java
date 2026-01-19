270c79c23f95a90cf3d4de9874284135de913774
shinwokkang <tlsdnr303@gmail.com>
2026-01-19 15:21:18 +0900

    endpoint /search -> search/keywords

--- src/main/java/com/mohaemukzip/mohaemukzip_be/domain/search/controller/SearchController.java
+++ src/main/java/com/mohaemukzip/mohaemukzip_be/domain/search/controller/SearchController.java
@@ -14,7 +14,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("/search")
+@RequestMapping("/search/keywords")
 @Tag(name = "Search")
 @Validated
 public class SearchController {
