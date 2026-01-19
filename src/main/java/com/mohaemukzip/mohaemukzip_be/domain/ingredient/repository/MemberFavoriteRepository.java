ecf3e162e811287230aaca39ba1fc4611acb2818
minjeong <litt22@g.hongik.ac.kr>
2026-01-19 17:18:51 +0900

    fix: 즐겨찾기 레포지토리 쿼리 설정

--- src/main/java/com/mohaemukzip/mohaemukzip_be/domain/ingredient/repository/MemberFavoriteRepository.java
+++ src/main/java/com/mohaemukzip/mohaemukzip_be/domain/ingredient/repository/MemberFavoriteRepository.java
@@ -1,13 +1,17 @@
 package com.mohaemukzip.mohaemukzip_be.domain.ingredient.repository;
 
-import com.mohaemukzip.mohaemukzip_be.domain.ingredient.entity.Ingredient;
 import com.mohaemukzip.mohaemukzip_be.domain.ingredient.entity.MemberFavorite;
 import com.mohaemukzip.mohaemukzip_be.domain.member.entity.Member;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 
 public interface MemberFavoriteRepository extends JpaRepository<MemberFavorite, Long> {
 
-    List<MemberFavorite> findAllByMember(Member member);
+    @Query("SELECT mf FROM MemberFavorite mf " +
+            "JOIN FETCH mf.ingredient " +
+            "WHERE mf.member = :member")
+    List<MemberFavorite> findAllByMember(@Param("member") Member member);
 }
