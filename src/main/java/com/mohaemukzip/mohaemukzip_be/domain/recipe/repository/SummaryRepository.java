ee52a18c3371503e5bacb023f8f3bc8b784305b7
shinwokkang <tlsdnr303@gmail.com>
2026-01-19 21:36:24 +0900

    refactor: SummaryRepositroy N+1 문제 Join FETCH 적용

--- src/main/java/com/mohaemukzip/mohaemukzip_be/domain/recipe/repository/SummaryRepository.java
+++ src/main/java/com/mohaemukzip/mohaemukzip_be/domain/recipe/repository/SummaryRepository.java
@@ -2,9 +2,13 @@ package com.mohaemukzip.mohaemukzip_be.domain.recipe.repository;
 
 import com.mohaemukzip.mohaemukzip_be.domain.recipe.entity.Summary;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 
 public interface SummaryRepository extends JpaRepository<Summary,Long> {
-    List<Summary> findByTitleContaining(String keyword);
+    
+    @Query("SELECT s FROM Summary s JOIN FETCH s.recipe WHERE s.title LIKE %:keyword%")
+    List<Summary> findByTitleContaining(@Param("keyword") String keyword);
 }
