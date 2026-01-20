55b590ca40bc2de307aeb90dfbdd1ee4a9299423
shinwokkang <tlsdnr303@gmail.com>
2026-01-20 15:16:47 +0900

    feat: Page Size final and 카테고리 존재 여부 검증 로직 추가

--- src/main/java/com/mohaemukzip/mohaemukzip_be/domain/recipe/service/RecipeQueryServiceImpl.java
+++ src/main/java/com/mohaemukzip/mohaemukzip_be/domain/recipe/service/RecipeQueryServiceImpl.java
@@ -3,7 +3,10 @@ package com.mohaemukzip.mohaemukzip_be.domain.recipe.service;
 import com.mohaemukzip.mohaemukzip_be.domain.recipe.converter.RecipeConverter;
 import com.mohaemukzip.mohaemukzip_be.domain.recipe.dto.RecipeResponseDTO;
 import com.mohaemukzip.mohaemukzip_be.domain.recipe.entity.Recipe;
+import com.mohaemukzip.mohaemukzip_be.domain.recipe.repository.CategoryRepository;
 import com.mohaemukzip.mohaemukzip_be.domain.recipe.repository.RecipeCategoryRepository;
+import com.mohaemukzip.mohaemukzip_be.global.exception.BusinessException;
+import com.mohaemukzip.mohaemukzip_be.global.response.code.status.ErrorStatus;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
@@ -16,10 +19,16 @@ import org.springframework.transaction.annotation.Transactional;
 public class RecipeQueryServiceImpl implements RecipeQueryService {
 
     private final RecipeCategoryRepository recipeCategoryRepository;
+    private final CategoryRepository categoryRepository;
+    private static final int PAGE_SIZE = 10;
 
     @Override
     public RecipeResponseDTO.RecipePreviewListDTO getRecipesByCategoryId(Long categoryId, Integer page) {
-        Page<Recipe> recipePage = recipeCategoryRepository.findRecipesByCategoryId(categoryId, PageRequest.of(page, 10));
+        if (!categoryRepository.existsById(categoryId)) {
+            throw new BusinessException(ErrorStatus.CATEGORY_NOT_FOUND);
+        }
+
+        Page<Recipe> recipePage = recipeCategoryRepository.findRecipesByCategoryId(categoryId, PageRequest.of(page, PAGE_SIZE));
         return RecipeConverter.toRecipePreviewListDTO(recipePage);
     }
 }
