f2874ade10047dd0eb6b3dfe5e3364ece6be8245
minjeong <litt22@g.hongik.ac.kr>
2026-01-22 14:05:45 +0900

    refactor: 재료 요청 업데이트 로직 리팩토링

--- src/main/java/com/mohaemukzip/mohaemukzip_be/domain/ingredient/repository/IngredientRequestRepository.java
+++ src/main/java/com/mohaemukzip/mohaemukzip_be/domain/ingredient/repository/IngredientRequestRepository.java
@@ -4,11 +4,7 @@ package com.mohaemukzip.mohaemukzip_be.domain.ingredient.repository;
 import com.mohaemukzip.mohaemukzip_be.domain.ingredient.entity.IngredientRequest;
 import com.mohaemukzip.mohaemukzip_be.domain.member.entity.Member;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Modifying;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 
-import java.time.LocalDateTime;
 import java.util.Optional;
 
 public interface IngredientRequestRepository extends JpaRepository<IngredientRequest, Long> {
@@ -16,7 +12,4 @@ public interface IngredientRequestRepository extends JpaRepository<IngredientReq
 
     Optional<IngredientRequest> findByMemberAndIngredientName(Member member, String ingredientName);
 
-    @Modifying(clearAutomatically = true)
-    @Query("UPDATE IngredientRequest ir SET ir.updatedAt = :now WHERE ir.id = :id")
-    void updateUpdatedAt(@Param("id") Long id, @Param("now") LocalDateTime now);
 }
--- src/main/java/com/mohaemukzip/mohaemukzip_be/domain/ingredient/service/IngredientCommandServiceImpl.java
+++ src/main/java/com/mohaemukzip/mohaemukzip_be/domain/ingredient/service/IngredientCommandServiceImpl.java
@@ -150,6 +150,7 @@ public class IngredientCommandServiceImpl implements IngredientCommandService {
     }
 
     @Override
+    @Transactional
     public void createIngredientRequest(Long memberId, IngredientRequestDTO.IngredientReq request) {
 
         Member member = memberRepository.findById(memberId)
@@ -159,10 +160,11 @@ public class IngredientCommandServiceImpl implements IngredientCommandService {
                 ingredientRequestRepository.findByMemberAndIngredientName(member, request.getIngredientName());
 
         if (existingRequest.isPresent()) {
-            ingredientRequestRepository.updateUpdatedAt(
-                    existingRequest.get().getId(),
-                    LocalDateTime.now()
-            );
+            IngredientRequest updateRequest = existingRequest.get();
+
+            updateRequest.renewCreatedDate();
+            ingredientRequestRepository.save(updateRequest);
+
             return;
         }
 
