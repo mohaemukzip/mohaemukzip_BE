c3b6c44ce27d30189dd7b64eb27c74f34dd17439
조은정/이현 <165488194+E-hyeon@users.noreply.github.com>
2026-01-31 01:17:48 +0900

    refactor: 홈 조회와 오늘의 미션 할당 트랜잭션 분리

--- src/main/java/com/mohaemukzip/mohaemukzip_be/domain/mission/service/MissionQueryServiceImpl.java
+++
@@ -1,9 +0,0 @@
-package com.mohaemukzip.mohaemukzip_be.domain.mission.service;
-
-import lombok.RequiredArgsConstructor;
-import org.springframework.stereotype.Service;
-
-@Service
-@RequiredArgsConstructor
-public class MissionQueryServiceImpl implements MissionQueryService {
-}
