5c05ef5d28be857f75ebe63fd2ad9269aa222088
shinwokkang <tlsdnr303@gmail.com>
2026-02-03 20:24:09 +0900

    refactor: /search api 요리명으로 조회되도록 수정

---
+++ src/main/java/com/mohaemukzip/mohaemukzip_be/domain/recipe/repository/DishProjection.java
@@ -0,0 +1,6 @@
+package com.mohaemukzip.mohaemukzip_be.domain.recipe.repository;
+
+public interface DishProjection {
+    Long getId();
+    String getName();
+}
--- src/main/java/com/mohaemukzip/mohaemukzip_be/domain/search/converter/SearchConverter.java
+++ src/main/java/com/mohaemukzip/mohaemukzip_be/domain/search/converter/SearchConverter.java
@@ -1,6 +1,7 @@
 package com.mohaemukzip.mohaemukzip_be.domain.search.converter;
 
 import com.mohaemukzip.mohaemukzip_be.domain.recipe.entity.Recipe;
+import com.mohaemukzip.mohaemukzip_be.domain.recipe.repository.DishProjection;
 import com.mohaemukzip.mohaemukzip_be.domain.recipe.repository.RecipeProjection;
 import com.mohaemukzip.mohaemukzip_be.domain.search.dto.SearchResponseDTO;
 import com.mohaemukzip.mohaemukzip_be.domain.search.dto.SearchResultDTO;
@@ -25,13 +26,20 @@ public class SearchConverter {
                 .build();
     }
 
+    public static SearchResultDTO toSearchResultDTO(DishProjection projection) {
+        return SearchResultDTO.builder()
+                .id(projection.getId())
+                .title(projection.getName())
+                .build();
+    }
+
     public static SearchResponseDTO toSearchResponseDTO(List<SearchResultDTO> results) {
         return SearchResponseDTO.builder()
                 .results(results)
                 .build();
     }
 
-    public static SearchResponseDTO toSearchResponseDTO(Page<RecipeProjection> page) {
+    public static SearchResponseDTO toSearchResponseDTO(Page<DishProjection> page) {
         List<SearchResultDTO> results = page.getContent().stream()
                 .map(SearchConverter::toSearchResultDTO)
                 .collect(Collectors.toList());
