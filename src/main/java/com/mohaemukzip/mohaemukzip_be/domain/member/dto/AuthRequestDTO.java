4c4575f646e503c81623502dfa183ff06b8b586c
minjeong <litt22@g.hongik.ac.kr>
2026-06-25 02:26:21 +0900

    feat: 회원가입 - loginId를 이메일 형식으로 변경

--- src/main/java/com/mohaemukzip/mohaemukzip_be/domain/member/dto/AuthRequestDTO.java
+++ src/main/java/com/mohaemukzip/mohaemukzip_be/domain/member/dto/AuthRequestDTO.java
@@ -2,6 +2,7 @@ package com.mohaemukzip.mohaemukzip_be.domain.member.dto;
 
 import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.Valid;
+import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints.Pattern;
@@ -11,11 +12,11 @@ import java.util.List;
 public class AuthRequestDTO {
     @Schema(description = "로그인 요청")
     public record LoginRequest(
-            @Schema(description = "로그인 ID", example = "admin")
+            @Schema(description = "로그인 ID", example = "test@naver.com")
             @NotBlank
             String loginId,
 
-            @Schema(description = "비밀번호", example = "mohaemukzip123!")
+            @Schema(description = "비밀번호", example = "Test1234!")
             @NotBlank
             String password
     ) { }
@@ -26,9 +27,9 @@ public class AuthRequestDTO {
             @NotBlank(message = "닉네임은 필수입니다")
             String nickname,
 
-            @Schema(description = "로그인 ID", example = "test01")
-            @NotBlank(message = "아이디는 필수입니다")
-            @Pattern(regexp = "^(?=.*[A-Za-z])[A-Za-z\\d]{4,20}$", message = "아이디는 영문과 숫자를 사용해 4자 이상 입력해주세요.")
+            @Schema(description = "이메일", example = "test@naver.com")
+            @NotBlank(message = "이메일은 필수입니다.")
+            @Email(message = "이메일 형식이 아닙니다.")
             String loginId,
 
             @Schema(description = "비밀번호", example = "Test1234!")
@@ -53,10 +54,9 @@ public class AuthRequestDTO {
     ) { }
 
     public record CheckLoginIdRequest(
-            @Schema(description = "아이디", example = "test01")
-            @NotBlank(message = "아이디를 입력해주세요.")
-            @Pattern(regexp = "^(?=.*[A-Za-z])[A-Za-z\\d]{4,20}$",
-                    message = "아이디는 영문, 숫자 4-20자로 입력해주세요.")
+            @Schema(description = "이메일", example = "test@naver.com")
+            @NotBlank(message = "이메일을 입력해주세요.")
+            @Email(message = "이메일 형식이 아닙니다.")
             String loginId
     ) {}
 
